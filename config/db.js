d8b0215c30b3bea4be774524ae58564ea9b4377e
Oqbolayev <javohiroqbolayev8039@gmail.com>
2025-06-01 13:06:58 +0500

    push qilindi

--- config/db.js
+++ config/db.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://javohir:8nVy0CxAQKZhmy9E82v1OUouppXYWqwK@dpg-d0tfoh63jp1c73ei5dn0-a.oregon-postgres.render.com/javohir',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-module.exports = pool;
